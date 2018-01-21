41faefd04b3016e427d37be4f58f081d08279d50
Matthew Wolff <mm.wolff@chi-squared.org>
2018-01-20 20:09:27 -0600

    complete push

--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /Users/matthew/github/theDNABot
-/Library/Frameworks/Python.framework/Versions/2.7/bin/python theDNABot.py
--- start_bot.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/tcsh
-cd /home/matthew/theDNABot/
-nohup python theDNABot.py >>& bot_log.out &
-cd ..
-exit
-done
-
